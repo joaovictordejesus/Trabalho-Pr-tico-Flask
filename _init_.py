a12ca13fddb12557846cad43ccd445be2bcf17c1
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2024-12-29 21:42:29 -0300

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -1,1 +1,14 @@
+#João Victor de Jesus Augusto
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+app = Flask(_name_)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/microblog.db'
+app.config['SECRET_KEY'] = "PDITA12345678"
+
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = "login"
+
+from app import routes
